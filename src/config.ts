0a1f8214290f7578797f015c9c7e3552824c10e4
drac94 <info@luisguerrero.me>
2022-03-24 17:03:18 -0600

    remove gitlab

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
-const { BACKEND_PORT, DB_URI, WORKERS, JWT_LIFE_TIME, JWT_SECRET } = process.env;
+const { PORT, MONGODB_URI, WORKERS, JWT_LIFE_TIME, JWT_SECRET } = process.env;
 
 export default {
   JwtLifeTime: JWT_LIFE_TIME || '20m',
   JwtSecret: JWT_SECRET || '',
-  MongoDbURI: DB_URI || '',
-  Port: BACKEND_PORT || 5000,
+  MongoDbURI: MONGODB_URI || '',
+  Port: PORT || 5000,
   Workers: WORKERS || 1,
 };
